579862c6cba78ab4f71573c4941122011b96681d
Nanakwameself <138698502+Nanakwameself@users.noreply.github.com>
2024-03-04 13:23:15 -0800

    update file

---
+++ GameScripts/Test1Script.cs
@@ -0,0 +1,12 @@
+using LoLSharp.Core;
+
+namespace LoLSharp.GameScripts
+{
+  [CustomScriptAttribute("Test1 Script",true)]
+  public class Test1Script : CustomScriptBase
+  {
+    public override void Update()
+    {
+    }
+  }
+}
